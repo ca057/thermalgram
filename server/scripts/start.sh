ccc64439443f58a996571f45a38911c56c053588
Christian Ost <christian@christianost.de>
2018-06-16 10:20:44 +0200

    add test file, improve startup script

--- server/scripts/start.sh
+++ server/scripts/start.sh
@@ -2,10 +2,20 @@
 
 echo "Starting THERMALGRAM..."
 
+# setting up serial port
+echo ""
+echo "Set up serial port with baud rate 19200..."
+sudo stty -F /dev/serial0 19200
+
+echo "Perform a test print..."
+sudo echo -e "THERMALGRAM test print\\n\\n\\n" > /dev/serial0
+
 # forward port 80 to 4321
+echo ""
 echo "Set up port forwarding :80 --> :4321..."
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 4321
 
 # start our server
+echo ""
 echo "Start server..."
 npm run start
